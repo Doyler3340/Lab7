15ba536c34b8b129f5964594c73fe68cd773a2be
Doyler3340 <72506331+Doyler3340@users.noreply.github.com>
2024-11-13 16:58:20 +0000

    Update movies.js

--- src/components/movies.js
+++ src/components/movies.js
@@ -1,11 +1,17 @@
+// Import the MovieItem component from the local "./movieitem" file
 import MovieItem from "./movieitem";
 
-const Movies = (props)=>{
-    return props.myMovies.map(
-        (movie)=>{
-            return <MovieItem mymovie={movie} key={movie._ID} />
-        }
-    );
+// Define the 'Movies' functional component which takes 'props' as an argument
+const Movies = (props) => {
+  // Map over the 'myMovies' prop (an array of movie objects) and render a MovieItem for each movie
+  return props.myMovies.map(
+    (movie) => {
+      // Return a MovieItem component for each movie, passing 'movie' as the 'mymovie' prop
+      // Use 'movie._ID' as the unique 'key' prop to help React identify each element efficiently
+      return <MovieItem mymovie={movie} key={movie._ID} />
+    }
+  );
 }
 
+// Export the 'Movies' component as the default export
 export default Movies;
